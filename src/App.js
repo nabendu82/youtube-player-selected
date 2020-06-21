c63c12838d1320db8998db6d2f0d22676afb9968
Nabendu <nabendu.biswas@gmail.com>
2020-06-21 19:39:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyBdVut9QCzqAHBzfDEh30yUp4E529som6s';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends Component {
   constructor(props){
